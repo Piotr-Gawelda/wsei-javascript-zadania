ac87c438f5c82515371f6b5fafc24f82e8c8f338
Piotr <piotr.gawelda@microsoft.wsei.edu.pl>
2021-01-26 16:05:21 +0100

    Variable Task

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,27 @@
+// //Zadanie1
+let randomNumber = 7;
+let randomText = "Random text";
+let numberWithText = 2 + "dwa";
+let logic = 4 < 2;
+let special = null;
+
+console.log(randomNumber);
+console.log(randomText);
+console.log(numberWithText);
+console.log(logic);
+console.log(special);
+
+
+// //Zadanie2
+
+let firstNumber = 2;
+let secNumber = 4;
+let sum = 0;
+sum = firstNumber + secNumber;
+console.log(sum)
+
+//Zadanie3
+
+let variable;
+console.log(variable)
+//Wynikiem zmiennej jest "undefined", ponieważ ma warotści
