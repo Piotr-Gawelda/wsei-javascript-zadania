7368aea891a6c7526a7495cb44437d03affbce5b
Piotr <piotr.gawelda@microsoft.wsei.edu.pl>
2021-01-26 15:53:11 +0100

    Beginning Task

---
+++ 1.Początki/script.js
@@ -0,0 +1,21 @@
+//Zadanie1
+// console.log("Piotr Gawełda");
+// Wyświetlenie w konsoli swojego imienia
+
+
+//Zadanie2
+	// console.log(12 + 32);
+// Wyświetlenie w konsoli wyniku dodawania (44)
+	// console.log(52 / 16);
+// Wyświetlenie w konsoli wyniku dzielenia (3.25)
+	// console.log(5 * 2);
+// Wyświetlenie w konsoli wyniku mnożenia (44)
+
+
+//Zadanie3
+	// console.log("Hello World");
+// Wyświetlenie w konsoli łańcuchu znaków Hello World
+	// console.log(Hello World);
+// Wyświetlenie blędu w konsoli, ponieważ nie ma takiej zmiennej
+	// console.log("Hello World);
+// Wyświetlenie blędu w konsoli, ponieważ brakuje domknięcia cudzysłów
